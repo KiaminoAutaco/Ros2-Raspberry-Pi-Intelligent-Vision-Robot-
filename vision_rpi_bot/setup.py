5fed13d2fbbc552300ed7b08a87b3740889edcbf
KiaminoAutaco <aymane8elhaoudar@gmail.com>
2025-09-26 20:13:48 +0000

    package to new branch

--- vision_rpi_bot/setup.py
+++ vision_rpi_bot/setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'vision_rpi_bot'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,17 +13,15 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='aymane',
-    maintainer_email='aymane@todo.todo',
+    maintainer='luqman',
+    maintainer_email='luqman@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'publisher_rpi_node = vision_rpi_bot.publisher:main',
+            'subscriber_rpi_node = vision_rpi_bot.subscriber:main',
         ],
     },
 )
